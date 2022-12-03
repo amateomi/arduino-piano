41c38b4adac075ff52c7a86d8a8e7b832548fa6b
Andrey Sikorin <amaomi.prog@gmail.com>
2022-12-03 22:54:58 +0300

    Add compile time note voltage computations

--- keyboard.cpp
+++ keyboard.cpp
@@ -2,17 +2,73 @@
 
 #include "utility.hpp"
 
-unsigned int Keyboard::mainOctaveVoltageToFrequency(int voltage) {
-  for (int i = 0; i < OCTAVE_NOTES_NUM; ++i) {
-    if (voltage < MAIN_OCTAVE_VOLTAGES[i])
-      return MAIN_OCTAVE_FREQUENCIES[i];
-  }
-  return 0;
+enum Resistor : long {
+  R0 = 10000,
+  R1 = 560,
+  R2 = 1300,
+  R3 = 2700,
+  R4 = 3900,
+  R5 = 5600,
+  R6 = 7500,
+  R7 = 10000,
+};
+
+[[nodiscard]] constexpr long sumParallelResistors(Resistor r1, Resistor r2) {
+  return r1 * r2 / float(r1 + r2);
+}
+
+enum class NoteResistance {
+  C4 = R1,
+  C4_SHARP = sumParallelResistors(R1, R2),
+  D4 = R2,
+  D4_SHARP = sumParallelResistors(R2, R3),
+  E4 = R3,
+  F4 = R4,
+  F4_SHARP = sumParallelResistors(R4, R5),
+  G4 = R5,
+  G4_SHARP = sumParallelResistors(R5, R6),
+  A4 = R6,
+  A4_SHARP = sumParallelResistors(R6, R7),
+  B4 = R7,
+};
+
+constexpr float ARDUINO_POWER_VOLTAGE = 5.0;
+constexpr int ARDUINO_MAX_ADC_VOLTAGE = 1024;
+
+constexpr int calculateMainOctaveVoltages(NoteResistance resistance) {
+  return static_cast<int>(resistance) / float(R0 + static_cast<int>(resistance))
+         * ARDUINO_MAX_ADC_VOLTAGE;
 }
 
 // TODO: calculate at compile time
-const int Keyboard::MAIN_OCTAVE_VOLTAGES[OCTAVE_NOTES_NUM]{ 38, 55, 84, 120, 196, 225, 259, 296, 322, 382, 464, 535 };
-const unsigned int Keyboard::MAIN_OCTAVE_FREQUENCIES[OCTAVE_NOTES_NUM]{ 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494 };
+const int Keyboard::MAIN_OCTAVE_VOLTAGES[OCTAVE_NOTES_NUM]{
+  calculateMainOctaveVoltages(NoteResistance::C4_SHARP),
+  calculateMainOctaveVoltages(NoteResistance::C4),
+  calculateMainOctaveVoltages(NoteResistance::D4_SHARP),
+  calculateMainOctaveVoltages(NoteResistance::D4),
+  calculateMainOctaveVoltages(NoteResistance::F4_SHARP),
+  calculateMainOctaveVoltages(NoteResistance::E4),
+  calculateMainOctaveVoltages(NoteResistance::G4_SHARP),
+  calculateMainOctaveVoltages(NoteResistance::F4),
+  calculateMainOctaveVoltages(NoteResistance::A4_SHARP),
+  calculateMainOctaveVoltages(NoteResistance::G4),
+  calculateMainOctaveVoltages(NoteResistance::A4),
+  calculateMainOctaveVoltages(NoteResistance::B4),
+};
+const Keyboard::NoteFrequency Keyboard::MAIN_OCTAVE_FREQUENCIES[OCTAVE_NOTES_NUM]{
+  Keyboard::NoteFrequency::C4_SHARP,
+  Keyboard::NoteFrequency::C4,
+  Keyboard::NoteFrequency::D4_SHARP,
+  Keyboard::NoteFrequency::D4,
+  Keyboard::NoteFrequency::F4_SHARP,
+  Keyboard::NoteFrequency::E4,
+  Keyboard::NoteFrequency::G4_SHARP,
+  Keyboard::NoteFrequency::F4,
+  Keyboard::NoteFrequency::A4_SHARP,
+  Keyboard::NoteFrequency::G4,
+  Keyboard::NoteFrequency::A4,
+  Keyboard::NoteFrequency::B4,
+};
 
 Keyboard& Keyboard::get() {
   static Keyboard instance;
@@ -23,8 +79,14 @@ Keyboard::Keyboard() {
   pinMode(PIN, INPUT);
   pinMode(UPPER_OCTAVE_PIN, INPUT_PULLUP);
   pinMode(LOWER_OCTAVE_PIN, INPUT_PULLUP);
+
   LOG("Keyboard: Created and atached to analog pin=%i, upper octave pin=%i, lower octave pin=%i",
       PIN, UPPER_OCTAVE_PIN, LOWER_OCTAVE_PIN);
+
+  LOG("Keyboard: Main octave computations (voltages must be in increasing order)");
+  for (int i = 0; i < OCTAVE_NOTES_NUM; ++i) {
+    LOG("Keyboard: voltage=%i, frequency=%i", MAIN_OCTAVE_VOLTAGES[i], MAIN_OCTAVE_FREQUENCIES[i]);
+  }
 }
 
 void Keyboard::updateCurrentOctave() {
@@ -56,4 +118,13 @@ unsigned int Keyboard::getNoteFrequency() const {
   }
 
   return frequency;
+}
+
+unsigned int Keyboard::mainOctaveVoltageToFrequency(int voltage) {
+  for (int i = 0; i < OCTAVE_NOTES_NUM; ++i) {
+    constexpr int reserve = 10;
+    if (MAIN_OCTAVE_VOLTAGES[i] - reserve < voltage && voltage < MAIN_OCTAVE_VOLTAGES[i] + reserve)
+      return MAIN_OCTAVE_FREQUENCIES[i];
+  }
+  return 0;
 }
--- keyboard.hpp
+++ keyboard.hpp
@@ -22,15 +22,29 @@ private:
   void pressUpperCallback();
 
   [[nodiscard]] static unsigned int mainOctaveVoltageToFrequency(int voltage);
-  [[nodiscard]] static constexpr int calculateMainOctaveVoltages(unsigned int noteResistor);
 
   static constexpr int PIN{ A5 };
   static constexpr int UPPER_OCTAVE_PIN{ 7 };
   static constexpr int LOWER_OCTAVE_PIN{ 8 };
 
+  enum NoteFrequency : unsigned int {
+    C4 = 262,
+    C4_SHARP = 277,
+    D4 = 294,
+    D4_SHARP = 311,
+    E4 = 330,
+    F4 = 349,
+    F4_SHARP = 370,
+    G4 = 392,
+    G4_SHARP = 415,
+    A4 = 440,
+    A4_SHARP = 466,
+    B4 = 494,
+  };
+
   static constexpr int OCTAVE_NOTES_NUM{ 12 };
   static const int MAIN_OCTAVE_VOLTAGES[OCTAVE_NOTES_NUM];
-  static const unsigned int MAIN_OCTAVE_FREQUENCIES[OCTAVE_NOTES_NUM];
+  static const NoteFrequency MAIN_OCTAVE_FREQUENCIES[OCTAVE_NOTES_NUM];
 
   int m_currentOctave{};
 
