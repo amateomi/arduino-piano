f5c5f626c46ae6ce77132488494ec14aa4eb1d94
Andrey Sikorin <amaomi.prog@gmail.com>
2023-01-13 15:02:18 +0300

    Move constants into class and implement getter

--- octave.cpp
+++ octave.cpp
@@ -1,14 +1,10 @@
-#include "button.hpp"
 #include "octave.hpp"
 
-constexpr auto LOWEST_OCTAVE = 1;
-constexpr auto MIDDLE_OCTAVE = 4;
-constexpr auto HIGHEST_OCTAVE = 7;
+#include <Arduino.h>
 
 Octave::Octave(uint8_t decreaseOctaveButtonPin,
                uint8_t increaseOctaveButtonPin)
-  : m_CurrentOctave{ MIDDLE_OCTAVE },
-    m_DecreaseOctaveButton{ decreaseOctaveButtonPin, INPUT_PULLUP },
+  : m_DecreaseOctaveButton{ decreaseOctaveButtonPin, INPUT_PULLUP },
     m_IncreaseOctaveButton{ increaseOctaveButtonPin, INPUT_PULLUP } {}
 
 void Octave::OnUpdate() {
@@ -20,6 +16,10 @@ void Octave::OnUpdate() {
       event == Button::Event::PRESS) {
     IncreaseOctave();
   }
+}
+
+int Octave::GetOctave() const {
+  return m_CurrentOctave;
 }
 
 void Octave::DecreaseOctave() {
