7fb1d6b8878d4e08cb7cbf223a38d457dd68b530
Andrey Sikorin <amaomi.prog@gmail.com>
2023-01-13 14:59:09 +0300

    Add buzzer

---
+++ buzzer.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stdint.h>
+
+#include "notes.hpp"
+#include "octave.hpp"
+
+class Buzzer {
+public:
+  explicit Buzzer(uint8_t pin);
+
+  void Play(Note, int octave);
+
+private:
+  [[nodiscard]] unsigned int MatchOctaveNoteWithFrequency(Note, int octave);
+
+  const uint8_t PIN;
+  Note m_LastPlayedNote{Note::NONE};
+  int m_LastPlayedOctave{Octave::MIDDLE_OCTAVE};
+};
