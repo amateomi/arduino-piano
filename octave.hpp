cc4a0bd53ac981c1e833ace0c2e397e4f462288e
Andrey Sikorin <amaomi.prog@gmail.com>
2023-01-13 15:05:47 +0300

    Add comment for getter

--- octave.hpp
+++ octave.hpp
@@ -14,13 +14,15 @@ public:
   // Update current octave each main loop tick
   void OnUpdate();
 
+  // Return current octave in range {1..7}
+  // where default (start value) is 4 (middle octave)
   [[nodiscard]] int GetOctave() const;
 
 private:
   void DecreaseOctave();
   void IncreaseOctave();
 
-  int m_CurrentOctave{MIDDLE_OCTAVE};
+  int m_CurrentOctave{ MIDDLE_OCTAVE };
   Button m_DecreaseOctaveButton;
   Button m_IncreaseOctaveButton;
 };
