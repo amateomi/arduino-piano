cc9287985b50086652b4163ff1a4689a768270b1
Andrey Sikorin <amaomi.prog@gmail.com>
2023-01-13 15:09:32 +0300

    Add const qualifiers and rearrange some fields

--- keyboard.hpp
+++ keyboard.hpp
@@ -6,11 +6,13 @@
 class Keyboard {
 public:
   // Resistance of all resistors in the keyboard circuit.
-  // R[0] is R1 and should be equal 10kOm.
-  // Resistance of R[1..7] should be sorted ascending.
-  using Resistors = unsigned long[8];
+  // R1 should be equal 10kOm.
+  // Resistance of R2-R8 should be sorted ascending.
+  struct Resistors {
+    unsigned long R1, R2, R3, R4, R5, R6, R7, R8;
+  };
 
-  Keyboard(Resistors resistors,
+  Keyboard(Resistors,
            uint8_t analogPin,
            uint8_t decreaseOctaveButtonPin,
            uint8_t increaseOctaveButtonPin);
@@ -19,30 +21,25 @@ public:
   void OnUpdate();
 
   // Return current note
-  [[nodiscard]] Note GetNote();
+  [[nodiscard]] Note GetNote() const;
 
   // Return current octave in range {1..7}
   // where default (start value) is 4 (middle octave)
-  [[nodiscard]] int GetOctave();
+  [[nodiscard]] int GetOctave() const;
 
 private:
   [[nodiscard]] int calculateRealVoltage(int adcVoltage);
-  [[nodiscard]] Note matchVoltageWithNote(int voltage);
+  [[nodiscard]] Note matchVoltageWithNote(int voltage) const;
 
-  [[nodiscard]] int calculateNoteVoltage(unsigned long noteResistance);
+  [[nodiscard]] int calculateNoteVoltage(unsigned long noteResistance) const;
 
   const Resistors RESISTORS;
-  const Note NOTES[NOTES_AMOUNT]{ Note::C4,
-                                  Note::C4_SHARP,
-                                  Note::D4,
-                                  Note::D4_SHARP,
+  const Note NOTES[NOTES_AMOUNT]{ Note::C4, Note::C4_SHARP,
+                                  Note::D4, Note::D4_SHARP,
                                   Note::E4,
-                                  Note::F4,
-                                  Note::F4_SHARP,
-                                  Note::G4,
-                                  Note::G4_SHARP,
-                                  Note::A4,
-                                  Note::A4_SHARP,
+                                  Note::F4, Note::F4_SHARP,
+                                  Note::G4, Note::G4_SHARP,
+                                  Note::A4, Note::A4_SHARP,
                                   Note::B4 };
   const int NOTES_VOLTAGE[NOTES_AMOUNT];
 
