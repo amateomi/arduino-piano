5bd2a052b4e8e6b477716b7366705ae89f47bae0
Andrey Sikorin <amaomi.prog@gmail.com>
2023-01-13 15:10:25 +0300

    Integrate logging and rearrange some lines

--- keyboard.cpp
+++ keyboard.cpp
@@ -1,5 +1,6 @@
-#include "notes.hpp"
 #include "keyboard.hpp"
+
+#include "logger.hpp"
 
 namespace {
 
@@ -15,28 +16,52 @@ Keyboard::Keyboard(Resistors resistors,
                    uint8_t decreaseOctaveButtonPin,
                    uint8_t increaseOctaveButtonPin)
   : RESISTORS{ resistors },
-    NOTES_VOLTAGE{ calculateNoteVoltage(RESISTORS[1]),
-                   calculateNoteVoltage(sumParallelResistors(RESISTORS[1], RESISTORS[2])),
-                   calculateNoteVoltage(RESISTORS[2]),
-                   calculateNoteVoltage(sumParallelResistors(RESISTORS[2], RESISTORS[3])),
-                   calculateNoteVoltage(RESISTORS[3]),
-                   calculateNoteVoltage(RESISTORS[4]),
-                   calculateNoteVoltage(sumParallelResistors(RESISTORS[4], RESISTORS[5])),
-                   calculateNoteVoltage(RESISTORS[5]),
-                   calculateNoteVoltage(sumParallelResistors(RESISTORS[5], RESISTORS[6])),
-                   calculateNoteVoltage(RESISTORS[6]),
-                   calculateNoteVoltage(sumParallelResistors(RESISTORS[6], RESISTORS[7])),
-                   calculateNoteVoltage(RESISTORS[7]) },
+    NOTES_VOLTAGE{ calculateNoteVoltage(RESISTORS.R2),
+                   calculateNoteVoltage(sumParallelResistors(RESISTORS.R2, RESISTORS.R3)),
+                   calculateNoteVoltage(RESISTORS.R3),
+                   calculateNoteVoltage(sumParallelResistors(RESISTORS.R3, RESISTORS.R4)),
+                   calculateNoteVoltage(RESISTORS.R4),
+                   calculateNoteVoltage(RESISTORS.R5),
+                   calculateNoteVoltage(sumParallelResistors(RESISTORS.R5, RESISTORS.R6)),
+                   calculateNoteVoltage(RESISTORS.R6),
+                   calculateNoteVoltage(sumParallelResistors(RESISTORS.R6, RESISTORS.R7)),
+                   calculateNoteVoltage(RESISTORS.R7),
+                   calculateNoteVoltage(sumParallelResistors(RESISTORS.R7, RESISTORS.R8)),
+                   calculateNoteVoltage(RESISTORS.R8) },
     ANALOG_PIN{ analogPin },
     m_Octave{ decreaseOctaveButtonPin, increaseOctaveButtonPin } {
+
+  const char* NOTES_NAME[NOTES_AMOUNT] = { "C4", "C4_SHARP",
+                                           "D4", "D4_SHARP",
+                                           "E4",
+                                           "F4", "F4_SHARP",
+                                           "G4", "G4_SHARP",
+                                           "A4", "A4_SHARP",
+                                           "B4" };
+
+  for (int i = 0; i < NOTES_AMOUNT; ++i) {
+    log("Note: %s\tFrequency: %u\tVoltage: %i",
+        NOTES_NAME[i],
+        static_cast<unsigned int>(NOTES[i]),
+        NOTES_VOLTAGE[i]);
+  }
+
+  log("Keyboard created");
 }
 
 void Keyboard::OnUpdate() {
   m_Octave.OnUpdate();
 
   const int adcVoltage = analogRead(ANALOG_PIN);
-  const int voltage = calculateRealVoltage(adcVoltage);
-  m_CurrentNote = matchVoltageWithNote(voltage);
+  m_CurrentNote = matchVoltageWithNote(adcVoltage);
+}
+
+Note Keyboard::GetNote() const {
+  return m_CurrentNote;
+}
+
+int Keyboard::GetOctave() const {
+  return m_Octave.GetOctave();
 }
 
 int Keyboard::calculateRealVoltage(int adcVoltage) {
@@ -45,7 +70,7 @@ int Keyboard::calculateRealVoltage(int adcVoltage) {
   return adcVoltage * POWER_VOLTAGE / MAX_ADC_VOLTAGE;
 }
 
-Note Keyboard::matchVoltageWithNote(int voltage) {
+Note Keyboard::matchVoltageWithNote(int voltage) const {
   constexpr auto DEVIATION_ERROR = 10;
   for (auto i = 0; i < NOTES_AMOUNT; ++i) {
     if (const auto delta = abs(NOTES_VOLTAGE[i] - voltage);
@@ -56,7 +81,7 @@ Note Keyboard::matchVoltageWithNote(int voltage) {
   return Note::NONE;
 }
 
-int Keyboard::calculateNoteVoltage(unsigned long noteResistance) {
-  constexpr auto POWER_VOLTAGE = 5;
-  return POWER_VOLTAGE * (static_cast<float>(noteResistance) / (RESISTORS[0] + noteResistance));
+int Keyboard::calculateNoteVoltage(unsigned long noteResistance) const {
+  constexpr auto POWER_VOLTAGE = 1024;
+  return POWER_VOLTAGE * (static_cast<float>(noteResistance) / (RESISTORS.R1 + noteResistance));
 }
