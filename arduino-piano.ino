0bee889218200c8e03c366ea8abb02880056cf28
Andrey Sikorin <amaomi.prog@gmail.com>
2023-01-13 15:12:06 +0300

    Add setup parameters and implement basic loop()

--- arduino-piano.ino
+++ arduino-piano.ino
@@ -1,12 +1,38 @@
-#include "notes.hpp"
+#include "logger.hpp"
 #include "keyboard.hpp"
+#include "buzzer.hpp"
+
+enum Pins : uint8_t {
+  ANALOG_KEYBOARD = A5,
+  DECREASE_OCTAVE_BUTTON = 8,
+  INCREASE_OCTAVE_BUTTON = 7,
+  BUZZER = 2,
+};
+Keyboard::Resistors resistors{ .R1 = 10000,
+                               .R2 = 560,
+                               .R3 = 1300,
+                               .R4 = 2700,
+                               .R5 = 3900,
+                               .R6 = 5600,
+                               .R7 = 7500,
+                               .R8 = 10000 };
 
 Keyboard* keyboard{};
+Buzzer* buzzer{};
 
 void setup() {
-  Keyboard::Resistors resistors{ 10000, 560, 1300, 2700, 3900, 5600, 7500, 10000 };
-  keyboard = new Keyboard{ A5, 8, 7, resistors };
+  log("Setup start");
+
+  keyboard = new Keyboard{ resistors,
+                           Pins::ANALOG_KEYBOARD,
+                           Pins::DECREASE_OCTAVE_BUTTON,
+                           Pins::INCREASE_OCTAVE_BUTTON };
+  buzzer = new Buzzer{ Pins::BUZZER };
+
+  log("Setup end");
 }
 
 void loop() {
+  keyboard->OnUpdate();
+  buzzer->Play(keyboard->GetNote(), keyboard->GetOctave());
 }
