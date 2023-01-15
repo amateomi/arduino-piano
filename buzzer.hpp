7fa9f50bd20fff9a59fcdc3d2bbadc6e208a0b5d
Andrey Sikorin <amaomi.prog@gmail.com>
2023-01-15 21:19:09 +0300

    Move `MatchNoteWithFrequency()` and implement Buzzer `Play` functions

--- buzzer.hpp
+++ buzzer.hpp
@@ -2,6 +2,7 @@
 
 #include "note.hpp"
 #include "octave.hpp"
+#include "melody-storage.hpp"
 
 class Buzzer {
 public:
@@ -13,20 +14,36 @@ public:
   }
 
   void Play(Note note, int octave) {
+    const auto frequency = MatchNoteWithFrequency(note, octave);
+    frequency != 0 ? tone(PIN, frequency) : noTone(PIN);
+
     const bool isChange = note != m_LastPlayedNote || octave != m_LastPlayedOctave;
     if (isChange) {
-      const auto frequency = Octave::MatchNoteWithFrequency(note, octave);
-      frequency ? tone(PIN, frequency) : noTone(PIN);
-
       m_LastPlayedNote = note;
       m_LastPlayedOctave = octave;
-
       log("Buzzer: play frequency=%u (octave=%i)", frequency, octave);
     }
   }
 
-  void PlayAlarm() const {
+  void Play(const Sound& sound) const {
+    sound.Frequency != 0 ? tone(PIN, sound.Frequency) : noTone(PIN);
+    delay(sound.DurationMs);
+    log("Buzzer: play sound{ frequency=%u, duration=%lums }",
+        sound.Frequency, sound.DurationMs);
+  }
 
+  void PlayAlarm() const {
+    Sound alarm;
+    alarm.Frequency = 1337;
+    alarm.DurationMs = 1000;
+    Play(alarm);
+  }
+
+  // Convert note to frequency according to octave
+  [[nodiscard]] static unsigned int MatchNoteWithFrequency(Note note, int octave) {
+    const auto frequency = static_cast<unsigned int>(note);
+    octave -= Octave::MIDDLE_OCTAVE;
+    return (octave < 0) ? frequency >> abs(octave) : frequency << octave;
   }
 
 private:
